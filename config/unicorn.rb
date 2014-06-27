c8391ca28158150480ed25a84fda19a4c5eea85f
yancheng <zqalyc@163.com>
2014-06-27 18:49:43 +0800

    部署配置

---
+++ config/unicorn.rb
@@ -0,0 +1,9 @@
+worker_processes 2
+
+APP_PATH = "/home/ubuntu/viicare"
+working_directory APP_PATH
+
+listen APP_PATH + "/tmp/unicorn.viicare.sock", :backlog => 64
+pid APP_PATH + "/tmp/pids/unicorn.pid"
+stderr_path APP_PATH + "/log/unicorn.stderr.log"
+stdout_path APP_PATH + "/log/unicorn.stderr.log"
