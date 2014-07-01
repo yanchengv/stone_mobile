3e370b7cd09eeca06bb1681f78c1c8216a783991
yancheng <zqalyc@163.com>
2014-07-01 18:41:26 +0800

    unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,16 @@
 worker_processes 2
 
+#APP_PATH = "/home/fitark/mimas"
 APP_PATH = "/home/ubuntu/deploy/viicare_deploy/current"
+
 working_directory APP_PATH
 
-listen APP_PATH + "/tmp/unicorn.viicare.sock", :backlog => 64
+#/tmp/unicorn.prometheus.sock;  这个sock来自 nginx的配置
+listen "/tmp/unicorn.viicare.sock", :backlog => 64 #这个sock来自 nginx的配置
+listen 8009
 pid APP_PATH + "/tmp/pids/unicorn.pid"
+# By default, the Unicorn logger will write to stderr.
+# Additionally, ome applications/frameworks log to stderr or stdout,
+# so prevent them from going to /dev/null when daemonized here:
 stderr_path APP_PATH + "/log/unicorn.stderr.log"
 stdout_path APP_PATH + "/log/unicorn.stderr.log"
