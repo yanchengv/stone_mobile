61330c32d7fed546ec8c169a45395d2e814ff77c
yancheng <zqalyc@163.com>
2014-07-02 09:48:56 +0800

    sercrets.yml

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Viicare
   class Application < Rails::Application
+    config.secret_key_base = YAML.load(File.open("#{Rails.root}/config/secrets.yml"))[Rails.env]['secret_key_base']
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
