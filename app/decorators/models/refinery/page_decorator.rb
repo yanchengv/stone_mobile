ba2db2edcc9c4fa25112b3fd2d9bcb78a9b4548c
yancheng <zqalyc@163.com>
2014-07-09 11:31:17 +0800

    首页中文bug

--- app/decorators/models/refinery/page_decorator.rb
+++ app/decorators/models/refinery/page_decorator.rb
@@ -2,11 +2,11 @@ Refinery::Page.class_eval do
   # attr_accessible :is_sticky_page, :is_top_page, :show_in_footer, :show_in_header,:sidebar_html
 
   def self.sticky_pages lang
-     # Refinery::Page.find_by_sql("select * from refinery_pages p,refinery_page_translations t where p.is_sticky_page=true AND t.locale='en'")
     # where :is_sticky_page => true
     #   Refinery::Page.where(:is_sticky_page => true)
     # Refinery::Page::Translation.where(:locale=>'zh-CN')
-    Refinery::Page.find_by_sql("select * from refinery_pages p,refinery_page_translations t where p.is_sticky_page=true AND t.locale='#{lang}' AND t.refinery_page_id=p.id")
+    # Refinery::Page.find_by_sql("select * from refinery_pages p,refinery_page_translations t where p.is_sticky_page=true AND t.locale='#{lang}' AND t.refinery_page_id=p.id")
+    Refinery::Page.find_by_sql("select * from refinery_pages where is_sticky_page=true AND id in (select refinery_page_id from refinery_page_translations where locale='#{lang}' )")
   end
   def self.top_pages
     where :is_top_page => true
