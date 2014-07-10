71caa603e821695b1320386ad799c2f1ab4e6af9
yancheng <zqalyc@163.com>
2014-07-10 17:15:37 +0800

    三级导航

---
+++ app/assets/javascripts/superfishinvert.js
@@ -0,0 +1,4 @@
+jQuery(document).ready(function(){
+    var nav = jQuery('#menu ul').superfish({
+        delay: 100
+    });});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,29 +2,14 @@ module ApplicationHelper
 
   def header_menu
     header_menu_items = Refinery::Menu.new @header_menu_pages
-     presenter = Refinery::Pages::MenuPresenter.new(header_menu_items, self)
-    presenter.first_css = nil
-    presenter.last_css = nil
+    presenter = Refinery::Pages::MenuPresenter.new(header_menu_items, self)
+    presenter.dom_id = 'menu'
+    presenter.css = 'menu clearfix'
+    presenter.list_item_tag = :li
+    presenter.selected_css = :current
+     presenter.max_depth = 2
+    presenter.list_tag_css = 'sf-menu'
     presenter
-    #presenter.first_css = nil
-    #presenter.last_css = nil
-
-    # presenter.dom_id = 'menu'
-    # presenter.css = 'menu clearfix'
-    # # presenter.menu_tag = :nav
-    # # presenter.list_tag = :ul
-    #  presenter.list_item_tag = :li
-    #  presenter.selected_css = :current
-    # # presenter.first_css = :first
-    # # presenter.last_css = :last
-    # # presenter.max_depth = 2
-    #presenter.list_tag_css = 'sf-menu'
-    #presenter
-    # value = presenter.render_html_without_child(@page.root.id)
-    # if !debug_mode
-    #   dc.set('headmenu'+@page.root.id.to_s, value)
-    # end
-    # return value
   end
 
   def footer_menu
