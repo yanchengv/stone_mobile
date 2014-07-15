4e7ab2e1fddf5c1b98f53854b067eb706d5f0827
yancheng <zqalyc@163.com>
2014-07-15 11:13:22 +0800

    show添加链接

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,7 +22,32 @@ module ApplicationHelper
     presenter.menu_tag = :div
     presenter
   end
-
+  def path_map
+    if @pathlist.nil?
+      pathlist = []
+      parent_root_page = @page.root
+      if parent_root_page.link_url != '/'
+        pathlist.append(parent_root_page)
+      end
+      if @page.parent_id == nil
+        return pathlist
+      end
+      if parent_root_page.id == @page.parent_id
+        pathlist.append(@page)
+        return pathlist
+      else
+        for midLevelItem in parent_root_page.children
+          if midLevelItem.id == @page.parent_id
+            pathlist.append(midLevelItem)
+          end
+        end
+        pathlist.append(@page)
+        return pathlist
+      end
+    else
+      return @pathlist
+    end
+  end
   def sticky_pages
     Refinery::Page.sticky_pages
   end
