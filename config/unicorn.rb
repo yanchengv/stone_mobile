a307f9f1463b67001086bba75f95dec84a2bd07a
yancheng <zqalyc@163.com>
2014-07-30 19:37:51 +0800

    unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,11 +3,12 @@ worker_processes 2
 #阿里部署
 # APP_PATH = "/home/ubuntu/deploy/viicare_deploy/current"
 #腾讯1服务器部署
-APP_PATH = "/dfs/deploy/stone_deploy/current"
+# APP_PATH = "/dfs/deploy/stone_deploy/current"
+APP_PATH = "/dfs/deploy/stone_mobile"
 working_directory APP_PATH
 
 #/tmp/unicorn.prometheus.sock;  这个sock来自 nginx的配置
-listen "/tmp/unicorn.stone.sock", :backlog => 64 #这个sock来自 nginx的配置
+listen APP_PATH+ "/tmp/unicorn.stone.sock", :backlog => 64 #这个sock来自 nginx的配置
 # listen 8009
 pid APP_PATH + "/tmp/pids/unicorn.pid"
 # By default, the Unicorn logger will write to stderr.
